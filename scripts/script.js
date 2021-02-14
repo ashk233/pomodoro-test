e1637558c359b61bb97f306cf373d5e9dd1547e8
Ashley Kou <43892820+ashk233@users.noreply.github.com>
2021-02-14 14:41:10 -0800

    set number of pomos

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,61 @@ start.addEventListener('click', function() {
     }
         
 });
+
+const pomo1 = document.getElementById('pomo1');
+const pomo2 = document.getElementById('pomo2');
+const pomo3 = document.getElementById('pomo3');
+const pomo4 = document.getElementById('pomo4');
+pomo1.addEventListener('click', function() {
+    if (pomo1.getAttribute('alt')=='yes') {
+        whitePomo(pomo1);
+        whitePomo(pomo2);
+        whitePomo(pomo3);
+        whitePomo(pomo4);
+    } else {
+        colorPomo(pomo1);
+    }
+});
+
+pomo2.addEventListener('click', function() {
+    if (pomo2.getAttribute('alt')=='yes') {
+        whitePomo(pomo2);
+        whitePomo(pomo3);
+        whitePomo(pomo4);
+    } else {
+        colorPomo(pomo1);
+        colorPomo(pomo2);
+    }
+});
+
+pomo3.addEventListener('click', function() {
+    if (pomo3.getAttribute('alt')=='yes') {
+        whitePomo(pomo3);
+        whitePomo(pomo4);
+    } else {
+        colorPomo(pomo1);
+        colorPomo(pomo2);
+        colorPomo(pomo3);
+    }
+});
+pomo4.addEventListener('click', function() {
+    if (pomo4.getAttribute('alt')=='yes') {
+        whitePomo(pomo4);
+    } else {
+        colorPomo(pomo1);
+        colorPomo(pomo2);
+        colorPomo(pomo3);
+        colorPomo(pomo4);
+    }
+});
+
+function colorPomo(pomo) {
+    pomo.setAttribute('alt','yes');
+    pomo.setAttribute('src','./assets/tomato_color.svg');
+        
+}
+
+function whitePomo(pomo) {
+    pomo.setAttribute('alt','no');
+    pomo.setAttribute('src','./assets/tomato_white.svg')
+}
